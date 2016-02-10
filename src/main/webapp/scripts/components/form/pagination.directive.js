72d0fd670de45f611f766320976bfd820a992cea
Julien Dubois <julien.dubois@gmail.com>
2016-02-10 10:31:48 +0100

    automatic project update

--- src/main/webapp/scripts/components/form/pagination.directive.js
+++
@@ -1,9 +0,0 @@
-/* globals $ */
-'use strict';
-
-angular.module('sampleNoCacheApp')
-    .directive('sampleNoCacheAppPagination', function() {
-        return {
-            templateUrl: 'scripts/components/form/pagination.html'
-        };
-    });

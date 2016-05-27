e3911f42758238eba522eff3ef6d0d18f5d28a75
Julien Dubois <julien.dubois@gmail.com>
2016-05-27 11:57:58 +0200

    automatic project update

--- src/main/webapp/app/layouts/navbar/active-menu.directive.js
+++ src/main/webapp/app/layouts/navbar/active-menu.directive.js
@@ -5,6 +5,8 @@
         .module('jhipsterNoCacheSampleApplicationApp')
         .directive('activeMenu', activeMenu);
 
+    activeMenu.$inject = ['$translate', '$locale', 'tmhDynamicLocale'];
+
     function activeMenu($translate, $locale, tmhDynamicLocale) {
         var directive = {
             restrict: 'A',

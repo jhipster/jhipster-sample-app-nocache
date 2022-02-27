6de48a24e3ff07feac4274bf7766186191b1d456
Pascal Grimaud <pascalgrimaud@gmail.com>
2022-02-27 09:12:58 +0100

    automatic project update

--- src/main/java/io/github/jhipster/sample/repository/UserRepository.java
+++ src/main/java/io/github/jhipster/sample/repository/UserRepository.java
@@ -4,8 +4,7 @@ import io.github.jhipster.sample.domain.User;
 import java.time.Instant;
 import java.util.List;
 import java.util.Optional;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.*;
 import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
@@ -16,13 +15,9 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
     Optional<User> findOneByActivationKey(String activationKey);
-
     List<User> findAllByActivatedIsFalseAndActivationKeyIsNotNullAndCreatedDateBefore(Instant dateTime);
-
     Optional<User> findOneByResetKey(String resetKey);
-
     Optional<User> findOneByEmailIgnoreCase(String email);
-
     Optional<User> findOneByLogin(String login);
 
     @EntityGraph(attributePaths = "authorities")
